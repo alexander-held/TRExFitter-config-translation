c0a9bedd64d83435252fc3ed8bf8a6789409e502
Alexander Held <alexander.held@cern.ch>
2019-12-15 21:59:15 -0500

    minor cleanup of utilities

--- util/util.py
+++ util/util.py
@@ -1,18 +1,23 @@
 def is_int(s):
+    """check whether input string can be converted to an integer"""
     try:
         int(s)
         return True
     except:
         return False
 
+
 def is_float(s):
+    """check whether input string can be converted to a float"""
     try:
         float(s)
         return True
     except:
         return False
 
+
 def translate_value(s):
+    """translate string to integer or float if applicable"""
     if is_int(s):
         return int(s)
     elif is_float(s):
